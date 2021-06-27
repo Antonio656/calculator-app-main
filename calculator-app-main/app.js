08661a8406ebed11e447dc5ea8f86e450a505f52
Antonio656 <51298456+Antonio656@users.noreply.github.com>
2021-06-27 12:13:10 +0200

    Add files via upload

---
+++ calculator-app-main/app.js
@@ -0,0 +1,92 @@
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector(".display-2");
+const tempResultEl = document.querySelector(".temp-result");
+const numbersEl = document.querySelectorAll(".number");
+const operationEl = document.querySelectorAll(".operation");
+const equalEl = document.querySelector(".equal");
+const clearAllEl = document.querySelector(".all-clear");
+const clearLastEl = document.querySelector(".last-entity-clear");
+
+
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
+
+//checking for dots//
+numbersEl.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === "." && haveDot) {
+      return;
+    }
+    // displaying numbers//
+    dis2Num += e.target.innerText;
+    display2El.innerText = dis2Num;
+  });
+});
+
+operationEl.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    if (!dis2Num) return;
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if (dis1Num && dis2Num && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(dis2Num);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+  });
+});
+
+function clearVar(name = "") {
+  dis1Num += dis2Num + " " + name + " ";
+  display1El.innerText = dis1Num;
+  display2El.innerText = "";
+  dis2Num = "";
+  tempResultEl.innerText = "";
+}
+
+function mathOperation() {
+  if (lastOperation === "*") {
+    result = parseFloat(result) * parseFloat(dis2Num);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(dis2Num);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(dis2Num);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(dis2Num);
+  }
+}
+
+equalEl.addEventListener("click", (e) => {
+  if (!dis1Num || !dis2Num) return;
+  haveDot = false;
+  mathOperation();
+  clearVar();
+  display2El.innerText = result;
+  tempResultEl.innerText = "";
+  dis2Num = result;
+  dis1Num = "";
+})
+
+clearAllEl.addEventListener("click", (e) => {
+  display1El.innerText = "";
+  display2El.innerText = "";
+  dis1Num = "";
+  dis2Num = "";
+  result = "";
+  tempResultEl.innerText = "";
+  haveDot = false;
+})
+
+clearLastEl.addEventListener("click", (e) => {
+  display2El.innerText = "";
+  dis2Num = "";
+  haveDot = false;
+});
+
